3b904e1e606e89f862500ec1d6f6240070375417
Willibaur <williambautista@gmail.com>
2017-10-20 20:42:18 +0100

    Add plugins and aliases

--- .oh-my-zsh/custom/plugins/aliases/aliases.plugin.zsh
+++ .oh-my-zsh/custom/plugins/aliases/aliases.plugin.zsh
@@ -1,8 +1,12 @@
 # Custom Aliases
 
+# Atom
+alias at='atom .'
+
 # Git
 alias gbdam='git branch | grep -v "master" | xargs git branch -D'
 alias gbdad='git branch | grep -v "develop" | xargs git branch -D'
+alias gbc='gcm && gbdam && clear && gl'
 
 # ZSH
 alias zshconfig='vim ~/.zshrc'

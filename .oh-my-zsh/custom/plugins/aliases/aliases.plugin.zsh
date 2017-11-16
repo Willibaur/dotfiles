9b857d7a1f50011eb53df822359557acb724f97f
Willibaur <williambautista@gmail.com>
2017-11-16 12:09:55 +0000

    Add script for killing specific 4567 port terminal

--- .oh-my-zsh/custom/plugins/aliases/aliases.plugin.zsh
+++ .oh-my-zsh/custom/plugins/aliases/aliases.plugin.zsh
@@ -8,6 +8,9 @@ alias gbdam='git branch | grep -v "master" | xargs git branch -D'
 alias gbdad='git branch | grep -v "develop" | xargs git branch -D'
 alias gbc='gcm && gbdam && clear && gl'
 
+# Terminal
+alias wwwkill='lsof -ti tcp:4567 | xargs kill -9'
+
 # ZSH
 alias zshconfig='vim ~/.zshrc'
 alias ohmyzsh='vim ~/dotfiles/.oh-my-zsh/custom'

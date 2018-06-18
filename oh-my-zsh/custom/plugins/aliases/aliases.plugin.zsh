6cea0f0b444938bd945751502ae6966009ace4fb
willibaur <williambautista@gmail.com>
2018-06-18 07:44:45 +0100

    Add Yarn aliases

--- oh-my-zsh/custom/plugins/aliases/aliases.plugin.zsh
+++ oh-my-zsh/custom/plugins/aliases/aliases.plugin.zsh
@@ -11,6 +11,24 @@ alias gbc='gcm && gbdam && clear && gl'
 # Terminal
 alias wwwkill='lsof -ti tcp:4567 | xargs kill -9'
 
+# Yarn
+alias y="yarn "
+alias ya="yarn add"
+alias yad="yarn add --dev"
+alias yac="yarn autoclean"
+alias ycc="yarn cache clean"
+alias yga="yarn global add"
+alias ygr="yarn global remove"
+alias yh="yarn help"
+alias yi="yarn init"
+alias yl="yarn list"
+alias yout="yarn outdated"
+alias ypr="yarn prune"
+alias yr="yarn run"
+alias yrm="yarn remove"
+alias yui="yarn upgrade-interactive --latest"
+alias yw="yarn why"
+
 # ZSH
 alias zshconfig='vim ~/.zshrc'
 alias ohmyzsh='vim ~/dotfiles/.oh-my-zsh/custom'

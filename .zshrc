66ed8229f5b625090a8df5792bf5f364f61718f9
willibaur <williambautista@gmail.com>
2018-08-26 00:22:59 +0100

    Rename zsh folder

--- .zshrc
+++ .zshrc
@@ -21,13 +21,14 @@ antigen bundle yarn
 antigen bundle z
 
 antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-history-substring-search
 antigen bundle zsh-users/zsh-syntax-highlighting
 
 # Load the theme.
-antigen bundle ~/dotfiles/oh-my-zsh/custom/themes muse_customized.zsh-theme --no-local-clone
+antigen bundle ~/dotfiles/zsh/custom/themes muse_customized.zsh-theme --no-local-clone
 
 # Load aliases.
-antigen bundle ~/dotfiles/oh-my-zsh/custom/plugins/aliases --no-local-clone
+antigen bundle ~/dotfiles/zsh/custom/plugins/aliases --no-local-clone
 
 # Tell Antigen that you're done.
 antigen apply
--- zsh/custom/plugins/aliases/aliases.plugin.zsh
+++ zsh/custom/plugins/aliases/aliases.plugin.zsh
@@ -8,24 +8,15 @@ alias gbdam='git branch | grep -v "master" | xargs git branch -D'
 alias gbdad='git branch | grep -v "develop" | xargs git branch -D'
 alias gbc='gcm && gbdam && clear && gl'
 
-# Terminal
+# Server
 alias wwwkill='lsof -ti tcp:4567 | xargs kill -9'
 
 # Yarn
-alias y="yarn "
-alias ya="yarn add"
-alias yad="yarn add --dev"
 alias yac="yarn autoclean"
-alias ycc="yarn cache clean"
 alias yga="yarn global add"
 alias ygr="yarn global remove"
-alias yh="yarn help"
 alias yi="yarn init"
-alias yl="yarn list"
-alias yout="yarn outdated"
 alias ypr="yarn prune"
-alias yr="yarn run"
-alias yrm="yarn remove"
 alias yui="yarn upgrade-interactive --latest"
 alias yw="yarn why"
 

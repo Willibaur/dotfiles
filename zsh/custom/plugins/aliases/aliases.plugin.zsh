689199bed7d4f8b1fa47b517a0f41c435620a646
willibaur <williambautista@gmail.com>
2019-03-09 10:12:32 +0000

    Add hub

--- zsh/custom/plugins/aliases/aliases.plugin.zsh
+++ zsh/custom/plugins/aliases/aliases.plugin.zsh
@@ -8,6 +8,10 @@ alias vc='code .'
 alias gbdam='git branch | grep -v "master" | xargs git branch -D'
 alias gbdad='git branch | grep -v "develop" | xargs git branch -D'
 alias gbc='gcm && gbdam && gfa && clear && gl'
+alias gbcn="git branch | grep \* | cut -d ' ' -f2"
+
+# Hub
+alias hprm="hub pr list -b master -f '%sC%>(8)%i %Cyellow%<(10)%au %Cgreen%<(15)%L %Creset%<(70)%t %Cyellow%>(50)%U %n'"
 
 # Server
 alias 3wkill='lsof -ti tcp:4567 | xargs kill -9'
